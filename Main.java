0154d64250f88d0ebb0c63403027d2d897227dd8
diegoforlin <135170390+diegoforlin@users.noreply.github.com>
2024-04-02 08:55:23 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,81 +1,66 @@
-import account.Account;
 import account.Login;
+import account.Account
 import java.math.BigDecimal;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main extends Account {
-    public static BigDecimal balance;
+public class Main {
+    private BigDecimal balance;
+    private BigDecimal limit;
+    private static final BigDecimal IOF_RATE = BigDecimal.valueOf(0.018); // Taxa de IOF de 1.8%
 
-    public Main(BigDecimal balance, BigDecimal limit, BigDecimal deposit) {
-        super(balance, limit, deposit);
-        Main.balance = balance;
-    }
-    static Login login = new Login();
-
-    @Override
-    public BigDecimal purchase() {
-        return purchase();
+    public Main(BigDecimal balance, BigDecimal limit) {
+        this.balance = balance;
+        this.limit = limit;
     }
 
-    public static void main (String[] args) throws ParseException {
+    public static void main(String[] args) throws ParseException {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         System.out.println("Bem-vindo ao banco. Digite o número da sua conta.");
         int accountNumber = sc.nextInt();
         sc.nextLine();
         String expirationDate = "15/10/2030";
-        Date expirationDateParsed = new SimpleDateFormat("dd/MM/yyyy").parse(expirationDate);
         System.out.println("A sua conta é " + accountNumber + " e seu cartão expira em " + expirationDate + " . Seja bem-vindo/a. Qual o seu nome?");
         String name = sc.nextLine();
         System.out.println("Bem-vindo/a " + name + ". Qual será o valor do depósito inicial na conta " + accountNumber + " ?");
         BigDecimal deposit = sc.nextBigDecimal();
         sc.nextLine();
-        Main main = new Main(deposit, BigDecimal.ZERO, deposit);
-        System.out.println("Depósito no valor de " + deposit + ". Seu saldo é de " + balance + " . Qual será o valor da compra? Lembrando que as taxa de IoF é de 1.8%.");
+        BigDecimal originalLimit = BigDecimal.valueOf(5000);
+        Main main = new Main(deposit, originalLimit);
+        System.out.println("Depósito no valor de " + deposit + ". Seu saldo é de " + main.getBalance() + " . Qual será o valor da compra? Lembrando que as taxa de IoF é de 1.8%.");
         BigDecimal purchaseAmount = sc.nextBigDecimal();
         main.purchase(purchaseAmount);
-        System.out.println("A compra foi no valor de " + purchaseAmount + " e seu saldo ficou no valor de " + balance + ". Por favor, digite a senha do cartão.");
+        System.out.println("A compra foi no valor de " + purchaseAmount + " e seu saldo ficou no valor de " + main.getBalance() + ". Por favor, digite a senha do cartão.");
+        Login login = new Login();
         login.auth();
         sc.nextLine();
         System.out.println("Senha aprovada. Compra autorizada no valor de " + purchaseAmount + ".");
         System.out.println();
-        System.out.println("Seu limite atual é de " + limit + ".");
+        System.out.println("Seu limite atual é de " + main.getLimit() + ".");
         sc.close();
     }
-    @Override
+
     public BigDecimal purchase(BigDecimal purchaseAmount) {
-        BigDecimal fees = setFees(purchaseAmount);
+        BigDecimal fees = calculateIOF(purchaseAmount);
         balance = balance.subtract(purchaseAmount).subtract(fees);
+        limit = limit.subtract(purchaseAmount).subtract(fees);
         return balance;
     }
 
-    @Override
-    public BigDecimal limit() {
-        BigDecimal newLimit = BigDecimal.valueOf(5000).subtract(purchase());
-        return newLimit;
+    private BigDecimal calculateIOF(BigDecimal purchaseAmount) {
+        return purchaseAmount.multiply(0.018);
     }
 
-    @Override
-    public BigDecimal limit(BigDecimal limit) {
-        return limit;
-    }
-
-    @Override
     public BigDecimal getBalance() {
-        return getBalance();
+        return balance;
     }
 
-    @Override
     public BigDecimal getLimit() {
-        return getLimit();
+        r	eturn limit;
     }
-
-    @Override
+	
     public void getAuth() {
-        getAuth();
     }
 }
